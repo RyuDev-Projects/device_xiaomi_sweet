0274140ffcea8fac13c186574ade7941982ecb75
romiyusnandar <yusromi04@gmail.com>
2025-03-10 13:07:16 +0000

    sweet: Drop inherriting dolby.mk
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- device.mk
+++ device.mk
@@ -20,9 +20,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
 
-# Call the dolby setup
-$(call inherit-product, vendor/oneplus/dolby/dolby.mk)
-
 # Call the MiuiCamera setup
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-sweet/device.mk)
 

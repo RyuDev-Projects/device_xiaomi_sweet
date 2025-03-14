e0c761e2883cf8851d882a414ecaee49be568ff7
romiyusnandar <yusromi04@gmail.com>
2025-03-14 01:31:27 +0000

    sweet: rro_overlays: Disable Pocket Lock
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- device.mk
+++ device.mk
@@ -285,6 +285,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    AOSPASweetFrameworksOverlay \
     SweetFrameworksOverlay \
     SweetSettingsOverlay \
     SweetSettingsProviderOverlay \

65a5cb4afcd84eadb8f83f69c34c3b5e4f8f8c2b
romiyusnandar <yusromi04@gmail.com>
2025-03-14 01:44:27 +0000

    sweet: Build CarrierConfigOverlay
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- device.mk
+++ device.mk
@@ -287,6 +287,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     AOSPASweetFrameworksOverlay \
+    CarrierConfigOverlay \
     SweetFrameworksOverlay \
     SweetSettingsOverlay \
     SweetSettingsProviderOverlay \

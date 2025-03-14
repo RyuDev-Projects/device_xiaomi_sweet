e72798d5d7bcd3afc1c7902e1b3723a6de79be62
romiyusnandar <yusromi04@gmail.com>
2025-03-14 02:46:12 +0000

    sweet: Don't append -qti to power HAL name
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- device.mk
+++ device.mk
@@ -294,7 +294,7 @@ TARGET_BOARD_PLATFORM := $(MSMSTEPPE)
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service
 
 # Properties
 include $(LOCAL_PATH)/properties.mk

a5e10a22788fefad87447c7ab4a7b6c402840009
romiyusnandar <yusromi04@gmail.com>
2025-03-14 02:21:04 +0000

    sweet: Drop unneccesary FCMs
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,6 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 # Manifest
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/manifests/device_framework_matrix.xml \
-    $(DEVICE_PATH)/manifests/lineage_device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/manifests/manifest.xml \
     hardware/qcom/media/conf_files/sm6150/c2_manifest.xml

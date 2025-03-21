c17e97c3a5b11e856bba44636595ac5e1036d6ae
romiyusnandar <yusromi04@gmail.com>
2025-03-21 09:58:57 +0000

    sweet: Switch to Common QTI Audio component
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- device.mk
+++ device.mk
@@ -48,14 +48,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl
 
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
-    audio.usb.default \
-    audio.r_submix.default
+    audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
-    libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
@@ -259,12 +254,7 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libOmxVidcCommon \
@@ -371,6 +361,7 @@ PRODUCT_COPY_FILES += \
 
 # QTI Components
 TARGET_COMMON_QTI_COMPONENTS := \
+    audio \
     display \
     gps
 
